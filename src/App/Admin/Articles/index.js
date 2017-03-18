54254ca9c4e355a2bcc34ca67b5dc9fb718edfb8
Toy <1659808224@qq.com>
2017-03-18 17:11:34 +0800

    修改输入改变方式

--- src/App/Admin/Articles/index.js
+++ src/App/Admin/Articles/index.js
@@ -5,20 +5,8 @@ import * as articles from 'lib/store/articles'
 import {connect} from 'react-redux'
 
 
-@connect(({articles})=>({articles}))
-export default class extends React.Component{
-	state={}
-	componentDidMount(){
-		this.setState(this.props.config)
-	}
-	render(){
-		var {address="",tel1="",tel2="",site="",mail="",fox=""}=this.state
-		return (
-			<div>
-				<div className={style.buttons}>
-					<RaisedButton label="测试" onClick={()=>articles.add({title:"233"})} />
-				</div>
-			</div>
-		)
-	}
-}
+export default connect(({articles})=>({data:articles}))(({data})=>(
+	<div>
+		<RaisedButton label="测试" />
+	</div>
+))
--- src/App/Admin/Config/index.js
+++ src/App/Admin/Config/index.js
@@ -1,30 +1,17 @@
 import React from 'react'
 import {TextField,Toolbar,RaisedButton} from 'material-ui'
 import style from '../index.styl'
-import {set as setConfig} from 'lib/store/config'
+import * as config from 'lib/store/config'
 import {connect} from 'react-redux'
 
 
-@connect(({config})=>({config}))
-export default class extends React.Component{
-	state={}
-	componentDidMount(){
-		this.setState(this.props.config)
-	}
-	render(){
-		var {address="",tel1="",tel2="",site="",mail="",fox=""}=this.state
-		return (
-			<div>
-				<TextField floatingLabelText="公司地址" className={style.input} value={address} onChange={(e,val)=>this.setState({address:val})} /><br />
-				<TextField floatingLabelText="加盟热线" className={style.input} value={tel1} onChange={(e,val)=>this.setState({tel1:val})} /><br />
-				<TextField floatingLabelText="服务热线" className={style.input} value={tel2} onChange={(e,val)=>this.setState({tel2:val})} /><br />
-				<TextField floatingLabelText="传真" className={style.input} value={fox} onChange={(e,val)=>this.setState({fox:val})} /><br />
-				<TextField floatingLabelText="网址" className={style.input} value={site} onChange={(e,val)=>this.setState({site:val})} /><br />
-				<TextField floatingLabelText="E－mail" className={style.input} value={mail} onChange={(e,val)=>this.setState({mail:val})} /><br />
-				<div className={style.buttons}>
-					<RaisedButton label="保存" onClick={()=>setConfig(this.state)} />
-				</div>
-			</div>
-		)
-	}
-}
+export default connect(({config})=>({data:config}))(({data})=>(
+	<div>
+		<TextField floatingLabelText="公司地址" className={style.input} value={data.address||""} onChange={(e,val)=>config.set({address:val})} /><br />
+		<TextField floatingLabelText="加盟热线" className={style.input} value={data.tel1||""} onChange={(e,val)=>config.set({tel1:val})} /><br />
+		<TextField floatingLabelText="服务热线" className={style.input} value={data.tel2||""} onChange={(e,val)=>config.set({tel2:val})} /><br />
+		<TextField floatingLabelText="传真" className={style.input} value={data.fox||""} onChange={(e,val)=>config.set({fox:val})} /><br />
+		<TextField floatingLabelText="网址" className={style.input} value={data.site||""} onChange={(e,val)=>config.set({site:val})} /><br />
+		<TextField floatingLabelText="E－mail" className={style.input} value={data.mail||""} onChange={(e,val)=>config.set({mail:val})} /><br />
+	</div>
+))
