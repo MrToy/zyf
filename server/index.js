12a8abfb26647f715d2186d795f660df29331422
Toy <1659808224@qq.com>
2017-03-30 13:30:24 +0800

    换成了koa

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
-require("babel-register")({
-	babelrc: false,
-	presets: [["env",{
-		node:"current"
-	}]]
+const Koa = require('koa')
+const serve = require('koa-serve-static')
+const logger = require('koa-logger')
+const renderToString = require('react-dom/server').renderToString
+const mount = require('koa-mount')
+const bundle = require('../dist/server.js').default
+
+const app = new Koa()
+const template=require('fs').readFileSync('./dist/index.html').toString()
+
+app.use(logger())
+app.use(mount('/assets',serve("./dist/assets",{maxAge:30*24*60*1000})))
+app.use(ctx=>{
+	const res = {status:200}
+	var html=renderToString(bundle(ctx.url,res))
+	if(res.url){
+		ctx.redirect(res.url)
+	}else {
+		ctx.body=template.replace("<!-- react-content -->",html)
+		ctx.status=res.status
+	}
 })
-require("./main")
+
+
+app.listen(8080)
