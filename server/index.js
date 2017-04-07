2624169e5930d1139d2c4496395d26e12f6ecded
Toy <1659808224@qq.com>
2017-04-07 14:49:21 +0800

    fix server route

--- server/index.js
+++ server/index.js
@@ -6,31 +6,45 @@ const {renderToString} = require('react-dom/server')
 const mount = require('koa-mount')
 const koaBody = require('koa-body')
 const Router = require('koa-router')
+const rewrite = require('koa-rewrite')
 const cors = require('koa-cors')
 const article = require('./article')
 const configs = require('./config')
 const user = require('./user')
 
+const apis= new Router()
+apis.use('/article',article.routes(),article.allowedMethods())
+apis.use('/config',configs.routes(),configs.allowedMethods())
+apis.use('/user',user.routes(),user.allowedMethods())
 
+const html=require('fs').readFileSync('./dist/index.html').toString()
+const router= new Router()
+router.get('/',ctx=>{
+	ctx.body=html
+})
+router.use('/api',apis.routes(),apis.allowedMethods())
 
-
-const router = new Router()
-router.use('/article',article.routes(),article.allowedMethods())
-router.use('/config',configs.routes(),configs.allowedMethods())
-router.use('/user',user.routes(),user.allowedMethods())
 
 const app = new Koa()
 app.use(logger())
 app.use(cors())
 app.use(mongo({uri: 'mongodb://mongo/zyf'}))
 app.use(koaBody({formidable:{uploadDir:'./upload',keepExtensions:true}}))
+app.use(rewrite('/admin*', '/'))
+app.use(rewrite('/about*', '/'))
+app.use(rewrite('/join*', '/'))
+app.use(rewrite('/strength*', '/'))
+app.use(rewrite('/foods*', '/'))
+app.use(rewrite('/news*', '/'))
+app.use(rewrite('/products*', '/'))
+app.use(rewrite('/skills*', '/'))
+app.use(rewrite('/shops*', '/'))
+app.use(rewrite('/contact*', '/'))
 app.use(mount('/assets',serve("./dist/assets",{maxAge:30*24*60*1000})))
 app.use(mount('/upload',serve("./upload",{maxAge:30*24*60*1000})))
-app.use(mount('/api',router.routes()))
-const html=require('fs').readFileSync('./dist/index.html').toString()
-app.use(async ctx=>{
-	ctx.body=html
-})
+app.use(router.routes())
+
+// app.use()
 
 
 app.listen(8080)
